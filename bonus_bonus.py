dc2efff38979067a9178a44aec7d8ac9f13cf06c
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-23 16:21:13 +0200

    .

--- bonus_bonus.py
+++ bonus_bonus.py
@@ -54,34 +54,19 @@ class RotationalMatrix3D:
         plt.grid(True)
         plt.show()
 
-    def animate(self, vector, duration=10):
+    def animate(self, vector, duration=10, frame_rate=30):
         """
-        Animates the rotation of the vector from the original position to the transformed position.
+        Animates the rotation from the original position to the transformed position.
         The animation lasts for the specified duration in seconds.
         """
-        num_frames = 100
-        frame_rate = num_frames / duration
+        num_frames = int(duration * frame_rate)
+        angles = np.linspace(0, 2 * np.pi, num_frames)
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
-        original_vector = vector.copy()
-        transformed_vector = self.__mul__(vector)
-
-        q = ax.quiver(0, 0, 0, original_vector[0], original_vector[1], original_vector[2], color='blue', label='Original')
-        q_animation = ax.quiver(0, 0, 0, transformed_vector[0], transformed_vector[1], transformed_vector[2], color='red', label='Transformed')
-
-        ax.set_xlim([-5, 5])
-        ax.set_ylim([-5, 5])
-        ax.set_zlim([-5, 5])
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        ax.set_title('3D Rotational Matrix Transformation')
-        ax.legend()
-        plt.grid(True)
-
-        angles = np.linspace(0, 2 * np.pi, num_frames)
+        q = ax.quiver(0, 0, 0, vector[0], vector[1], vector[2], color='blue', label='Original')
+        q_animation = ax.quiver(0, 0, 0, vector[0], vector[1], vector[2], color='red', label='Transformed')
 
         def update_frame(frame):
             roll = self.roll * angles[frame] / (2 * np.pi)
@@ -92,30 +77,29 @@ class RotationalMatrix3D:
 
             transformed_vector = R * vector
 
-            q_animation.set_UVC(transformed_vector[0], transformed_vector[1], transformed_vector[2])
+            q_animation.set_segments([(np.array([0, 0, 0]), transformed_vector)])
 
-            return q_animation,
+            return q_animation
 
-        animation = FuncAnimation(fig, update_frame, frames=num_frames, interval=1000 / frame_rate, blit=True)
+        animation = FuncAnimation(fig, update_frame, frames=num_frames, interval=1000 / frame_rate, blit=False)
+
+        ax.set_xlim([-5, 5])
+        ax.set_ylim([-5, 5])
+        ax.set_zlim([-5, 5])
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.set_title('3D Rotational Matrix Transformation Animation')
+        ax.legend()
+
+        plt.grid(True)
         plt.show()
 
-
-# Example usage
 roll = np.pi / 4
 pitch = np.pi / 3
 yaw = np.pi / 6
 
 vector = np.array([1, 2, 3])
 
-# Create an instance of the RotationalMatrix3D class with the given roll, pitch, and yaw
 rot_matrix = RotationalMatrix3D(roll, pitch, yaw)
-
-# Apply the rotation matrix to the vector
-transformed_vector = rot_matrix * vector
-
-# Visualize the original and transformed vectors
-rot_matrix.visualize(vector)
-
-# Animate the rotation from the original position to the transformed position
-rot_matrix.animate(vector, duration=10)
-
+rot_matrix.animate(vector, duration=10, frame_rate=30)
